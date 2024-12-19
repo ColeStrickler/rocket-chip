c916dad62cf10f366b43ea800587f127c3d6eea9
ColeStrickler <colestrickler@gmail.com>
2024-12-18 21:33:42 -0600

    mbus attachment

--- src/main/scala/subsystem/MemoryBus.scala
+++ src/main/scala/subsystem/MemoryBus.scala
@@ -9,11 +9,12 @@ import freechips.rocketchip.devices.tilelink.{BuiltInDevices, HasBuiltInDevicePa
 import freechips.rocketchip.tilelink.{
   ReplicatedRegion, HasTLBusParams, HasRegionReplicatorParams, TLBusWrapper,
   TLBusWrapperInstantiationLike, RegionReplicator, TLXbar, TLInwardNode,
-  TLOutwardNode, ProbePicker, TLEdge, TLFIFOFixer, TLWidthWidget
+  TLOutwardNode, ProbePicker, TLEdge, TLFIFOFixer, TLWidthWidget, TLFragmenter
 }
 import freechips.rocketchip.util.Location
 import subsystem.rme.RME
 import subsystem.rme.RelMemParams
+import midas.targetutils.SynthesizePrintf
 
 /** Parameterization of the memory-side bus created for each memory channel */
 case class MemoryBusParams(
@@ -45,20 +46,34 @@ class MemoryBus(params: MemoryBusParams, name: String = "memory_bus")(implicit p
     r.prefix := addressPrefixNexusNode
     addressPrefixNexusNode
   }
+  //val rme = None
+  // LETS TRY TO CHANGE THE RME BASE ADDRESS, WE GOT ERROR ON BOOT last time
+ val rme = Some(LazyModule(new RME(RelMemParams(0x2000000,  0xe0000000L, this))))
 
-  val rme = Some(LazyModule(new RME(RelMemParams(0x2000000,  0x70000000))))
-
-  private val xbar = LazyModule(new TLXbar(nameSuffix = Some(name))).suggestName(busName + "_xbar")
+  val xbar = LazyModule(new TLXbar(nameSuffix = Some(name))).suggestName(busName + "_xbar")
   val inwardNode: TLInwardNode =
     replicator.map(xbar.node :*=* TLFIFOFixer(TLFIFOFixer.all) :*=* _.node)
         .getOrElse(xbar.node :*=* TLFIFOFixer(TLFIFOFixer.all))
+
+
+ 
   
+ // rme.get.manager :*= xbar.node
+
+
   /* 
     Is this the correct way to do this?
   */
-  //coupleTo("rme-manager") {rme.get.manager := TLWidthWidget(this.beatBytes) := _ }
-
+  //coupleTo("rme-manager") {rme.get.manager := _ }
+  //coupleFrom()
+    /* 
+      Can we make the :*= into a := ?
+     */
+    
+  //val outwardNode: TLOutwardNode = rme.get.node :*= ProbePicker() :*= xbar.node
   val outwardNode: TLOutwardNode = rme.get.node :*= ProbePicker() :*= xbar.node
+   //val outwardNode: TLOutwardNode = ProbePicker() :*= xbar.node
+  // coupleTo("rme-manager"){ rme.get.manager := TLFragmenter(beatBytes, blockBytes) := _ }
   def busView: TLEdge = xbar.node.edges.in.head
 
   val builtInDevices: BuiltInDevices = BuiltInDevices.attach(params, outwardNode)
